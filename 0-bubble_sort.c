731984c97b3a35a67b811026e334b2f248160295
Jose Torres <jose.torres122@outlook.com>
2021-10-26 17:00:58 +0000

    Be the first attempt at completing Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function
+ * @array: parameter
+ * @size: parameter
+ * Return: 0
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t idx;
+	int temp;
+	int swapped;
+
+	while(1)
+	{
+		swapped = 0;
+		for (idx = 0 ; idx < size ; idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+
+		if (swapped == 0)
+			break;
+	}
+}
