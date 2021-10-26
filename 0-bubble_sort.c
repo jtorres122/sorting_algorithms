ccc952594a47e05937372b215a9e6fb2c3a13960
Jose Torres <jose.torres122@outlook.com>
2021-10-26 17:20:59 +0000

    Add edge case for when size < 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,25 +14,23 @@ void bubble_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	else
-	{
-		while (1)
-		{
-			swapped = 0;
-			for (idx = 0 ; idx < size - 1 ; idx++)
-			{
-				if (array[idx] > array[idx + 1])
-				{
-					temp = array[idx];
-					array[idx] = array[idx + 1];
-					array[idx + 1] = temp;
-					print_array(array, size);
-					swapped = 1;
-				}
-			}
 
-			if (swapped == 0)
-				break;
+	while (1)
+	{
+		swapped = 0;
+		for (idx = 0 ; idx < size - 1 ; idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
